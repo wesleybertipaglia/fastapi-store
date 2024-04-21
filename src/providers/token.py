c0119d884642e7d773154f9dc1ac72292cd0b3e8
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-04-21 20:36:28 -0300

    feat: add token

--- src/providers/token.py
+++ src/providers/token.py
@@ -8,6 +8,7 @@ class Token:
         self.secret_key = "17f8341226dbfd4f2ef424c64a475f1bb2e7c9b0"
         self.algorithm = "HS256"
         self.access_token_expire_minutes = 30
+        self.blacklist = []
 
     def create_access_token(self, data: dict):
         copy_data = data.copy()
@@ -15,8 +16,17 @@ class Token:
         expire_timestamp = expire.timestamp()
         copy_data.update({"exp": expire_timestamp})
         return jwt.encode(copy_data, self.secret_key, algorithm=self.algorithm)
+    
+    def revoke_access_token(self, token: str):
+        self.blacklist.append(token)
+        expire = datetime.now()
+        expire_timestamp = expire.timestamp()        
+        return jwt.encode({"exp": expire_timestamp}, self.secret_key, algorithm=self.algorithm)
         
-    def verify_token(self, token: str):
+    def verify_token(self, token: str):        
+        if token in self.blacklist:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token revoked")
+
         try:
             payload = jwt.decode(token, self.secret_key, algorithms=[self.algorithm])
             return payload.get("sub")
