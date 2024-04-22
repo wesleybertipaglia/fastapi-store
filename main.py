b373a97f2b98baaf55da170a36665f642a705643
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-04-21 22:45:33 -0300

    feat: add timer middleware

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from src.config.database import create_db
 from src.routes.user import router as user_router
@@ -7,6 +7,7 @@ from src.routes.product import router as product_router
 from src.routes.order import router as order_router
 from src.routes.auth import router as auth_router
 from src.routes.profile import router as profile_router
+from src.middlewares.timer import time_middleware
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000)
@@ -31,8 +32,15 @@ app.add_middleware(
 def home():
     return 'Welcome to blx API 🎉'
 
+# routes
 app.include_router(auth_router, prefix='/auth')
 app.include_router(profile_router, prefix='/profile')
 app.include_router(user_router, prefix='/users')
 app.include_router(product_router, prefix='/products')
 app.include_router(order_router, prefix='/orders')
+
+# middlewares
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    response = await time_middleware(request, call_next)
+    return response
