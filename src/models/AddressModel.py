adaee1298e1a921401bfd1c368b1841f926e7e98
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-05-22 21:19:12 -0300

    feat: add address and payment method models

---
+++ src/models/AddressModel.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Integer, Float, ForeignKey, String
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from src.config.database import Base
+from uuid import uuid4
+
+class AddressModel(Base):
+    __tablename__ = 'addresses'
+    
+    id: Mapped[str] = mapped_column(String, primary_key=True, index=True, default=lambda: str(uuid4()))
+    user_id: Mapped[str] = mapped_column(String, ForeignKey('users.id'))
+    address: Mapped[str] = mapped_column(String, nullable=False)
+    city: Mapped[str] = mapped_column(String, nullable=False)
+    state: Mapped[str] = mapped_column(String, nullable=False)
+    zip_code: Mapped[str] = mapped_column(String, nullable=False)
+    
+    user: Mapped['UserModel'] = relationship('UserModel', back_populates='addresses')
+    order_shipping: Mapped['OrderShippingModel'] = relationship('OrderShippingModel', back_populates='address')    
---
+++ src/models/PaymentMethodModel.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Integer, Float, ForeignKey, String
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from src.config.database import Base
+from uuid import uuid4
+
+class PaymentMethodModel(Base):
+    __tablename__ = 'payment_methods'
+    
+    id: Mapped[str] = mapped_column(String, primary_key=True, index=True, default=lambda: str(uuid4()))
+    user_id: Mapped[str] = mapped_column(String, ForeignKey('users.id'))
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    description: Mapped[str] = mapped_column(String, nullable=False)
+    type: Mapped[str] = mapped_column(String, nullable=False, )
+    
+    user: Mapped['UserModel'] = relationship('UserModel', back_populates='payment_methods')
+    orders: Mapped['OrderModel'] = relationship('OrderModel', back_populates='payment_method')
