d54962b97575432328303ddb7020eaeedf0c2b59
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-05-25 18:39:49 -0300

    fix: change database name

--- src/config/database.py
+++ src/config/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgresql://admin:admin@localhost:5432/database")
+engine = create_engine("postgresql://admin:admin@localhost:5432/store")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
